88855999d0d75ecd82f15f9f89e7d74c417996a0
cgreg4000 <calebggregory@gmail.com>
2022-03-24 15:53:23 -0500

    people category displays name of homeworld

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -4,36 +4,49 @@ import axios from "axios";
 
 const People = () => {
 
-    const {id} = useParams();
+    const { id } = useParams();
     let [person, setPerson] = useState({});
+    let [planet, setPlanet] = useState({});
     let [isError, setIsError] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get(`https://swapi.dev/api/people/${id}`)
             .then(peopleResponse => {
                 setPerson(peopleResponse.data);
                 console.log(peopleResponse.data);
+                console.log("Got the person")
+                axios.get(peopleResponse.data.homeworld)
+                    .then(planetResponse => {
+                        console.log("Got the planet")
+                        console.log(planetResponse)
+                        setPlanet(planetResponse.data)
+                    })
+                    .catch(err => {
+                        console.log("error getting planet")
+                        console.log(err)
+                    })
             })
             .catch(err => {
                 setIsError(true);
+                console.log("error getting person")
                 console.log(err);
             })
-            setIsError(false);
-            setPerson("");
+        setIsError(false);
+        setPerson("");
     }, [id])
-    
-    return(
-            isError === true ?
+
+    return (
+        isError === true ?
             <div>
                 <h1 className="mb-3">These aren't the droids you're looking for.</h1>
                 <img src="https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/05/ANH-Ben-identification.jpg" alt="Obi-Wan Kenobi" />
-            </div>: 
+            </div> :
             <div>
                 <h1 className="mb-3">{person.name}</h1>
                 <p>Height: {person.height} cm</p>
                 <p>Mass: {person.mass} kg</p>
                 <p>Birth Year: {person.birth_year}</p>
-                <p>Hair Color: {person.hair_color}</p>
+                <p>Home World: {planet.name}</p>
             </div>
     )
 }
